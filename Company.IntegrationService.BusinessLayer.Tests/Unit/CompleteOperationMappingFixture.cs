bf4590fff2cd1ff7d5a9f4d57b1bec4dfa16b118
Darren Neimke <darren.neimke@gmail.com>
2013-01-12 21:53:23 +1030

    Adding a sample testing project to do Unit and Behavioural testing

---
+++ Company.IntegrationService.BusinessLayer.Tests/Unit/CompleteOperationMappingFixture.cs
@@ -0,0 +1,35 @@
+﻿using Company.IntegrationService.BusinessLayer.Components.LoansService;
+using Company.IntegrationService.Contracts.MessageContracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Company.IntegrationService.BusinessLayer.Tests.Unit
+{
+    [TestClass]
+    public class CompleteOperationMappingFixture
+    {
+        CompleteProcessRequestComponentMappings mappings;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mappings = new CompleteProcessRequestComponentMappings();
+        }
+
+        [TestMethod]
+        public void ShouldMapFromCompleteRequestToCustomerLoans()
+        {
+            // Arrange
+            var request = new CompleteRequest
+            {
+                Applicant = new Contracts.DataContracts.Applicant { Name = "Fred" },
+                ShouldSucceed = true
+            };
+
+            // Act
+            var result = mappings.CreateCustomerLoansFromCompleteRequest(request);
+        
+            // Assert
+            Assert.AreEqual("Fred", result.Customer.Name);
+        }
+    }
+}
--- Company.IntegrationService.BusinessLayer/Components/LoansService/CompleteProcessRequestComponent.cs
+++ Company.IntegrationService.BusinessLayer/Components/LoansService/CompleteProcessRequestComponent.cs
@@ -12,6 +12,7 @@ namespace Company.IntegrationService.BusinessLayer.Components.LoansService
     {
         // Manages all of its own dependencies
         private readonly ILoansClientProxy loansClient = null;
+        private readonly CompleteProcessRequestComponentMappings mappings;
 
         public CompleteProcessRequestComponent(ILoansClientProxy loansClient)
         {
@@ -24,64 +25,11 @@ namespace Company.IntegrationService.BusinessLayer.Components.LoansService
         // The process operation runs all of the steps to process the request
         public CompleteResponse Process(CompleteRequest request)
         {
-            var customerLoans = CompleteOperationMapping.CreateCustomerLoansFromCompleteRequest(request);
+            var customerLoans = mappings.CreateCustomerLoansFromCompleteRequest(request);
             var result = loansClient.Upload(customerLoans);
 
-            var response = CompleteOperationMapping.PrepareResponseType(request, result);
+            var response = mappings.PrepareResponseType(request, result);
             return response;
         }
-
-
-        #region Everything here is the logic for the steps that are carried out by this process component
-
-
-        #endregion
-
-
-        #region Mappings for this process
-        private static class CompleteOperationMapping
-        {
-            /// <summary>
-            /// Creates a new CustomerLoans instance with values mapped from an incoming CompleteRequest
-            /// </summary>
-            /// <param name="request">The CompleteRequest input value</param>
-            /// <returns>A valid CustomerLoans instance</returns>
-            internal static CustomerLoans CreateCustomerLoansFromCompleteRequest(CompleteRequest request)
-            {
-                return new CustomerLoans
-                {
-                    Customer = new CustomerAccount { Name = request.Applicant.Name, Id = 1 },
-                    Loans = new List<Loan>
-                    {
-                        new Loan
-                        {
-                            Id = 1,
-                            LoanType = LoanType.A
-                        }
-                    }
-                };
-            }
-
-
-            /// <summary>
-            /// Prepares the response message based on operations and inputs
-            /// </summary>
-            /// <param name="request">The initial request</param>
-            /// <param name="loanNumber">A loan number domain entity from the LoanManagement domain</param>
-            /// <returns></returns>
-            internal static CompleteResponse PrepareResponseType(CompleteRequest request, LoanNumber loanNumber)
-            {
-                return new CompleteResponse
-                {
-                    LoanApplication = new LoanApplication
-                    {
-                        Applicant = new Applicant { Name = request.Applicant.Name },
-                        IsApproved = request.ShouldSucceed,
-                        ApprovalMessage = (request.ShouldSucceed) ? string.Format("Loan Number is {0}", loanNumber.Value) : "Should not succeed based on input parameters"
-                    }
-                };
-            }
-        }
-        #endregion
     }
 }
